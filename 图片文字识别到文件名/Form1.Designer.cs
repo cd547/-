e7e449ec75bc7edacdd4ea6f07d366512c8e01c7
Administrator <415963856@qq.com>
2019-03-23 17:05:25 +0800

    添加项目文件。

---
+++ 图片文字识别到文件名/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace 图片文字识别到文件名
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(194, 146);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(438, 118);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 232);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(113, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "裁剪图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(194, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(438, 51);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "订单编号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "/";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "打开文件夹";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(227, 293);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "批量识别";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("宋体", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(14, 322);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(618, 244);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "0";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(550, 293);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "生成";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(441, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(95, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "文件数：";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(308, 300);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(115, 10);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 570);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ 图片文字识别到文件名/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace 图片文字识别到文件名
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable dt;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+        }
+
+        string fn;
+        string[] files = null;
+        private string path = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog=new OpenFileDialog();
+            
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fn = openFileDialog.FileName;
+                this.label2.Text = fn;
+                //获取用户选择文件的后缀名 
+                string extension = Path.GetExtension(fn);
+                var img = new Bitmap(fn);
+                //声明允许的后缀名 
+                string[] str = new string[] { ".jpg", ".png" };
+                this.pictureBox1.Load(fn);
+                if (!str.Contains(extension))
+                {
+                    MessageBox.Show("仅能上传jpg,png格式的图片！");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //识别图片文字
+            var img = new Bitmap(fn);
+            var ocr = new TesseractEngine("./tessdata", "chi_sim",EngineMode.Default);
+            var page = ocr.Process(Cut(img, 100, 584, 3687, 713));
+             
+            //using (var page = ocr.Process(Cut(img, 1869, 561, 1865, 661)))
+            {
+                this.textBox1.Text = page.GetText().Replace(" ", "");
+                page.Dispose();
+                ocr.Dispose();
+            }
+
+            //查找订单编号
+            
+            int start=this.textBox1.Text.IndexOf("订单编号");
+            if (start < 0)
+            {
+                this.label1.Text = "无";
+            }
+            else
+            {
+                this.label1.Text = "订单编号：" + this.textBox1.Text.Substring(start + 5, 10);
+            }
+            
+           
+          
+
+
+        }
+
+
+        /// <summary>  
+        /// 剪裁 -- 用GDI+   
+        /// </summary>  
+        /// <param name="b">原始Bitmap</param>  
+        /// <param name="StartX">开始坐标X</param>  
+        /// <param name="StartY">开始坐标Y</param>  
+        /// <param name="iWidth">宽度</param>  
+        /// <param name="iHeight">高度</param>  
+        /// <returns>剪裁后的Bitmap</returns>  
+        public  Bitmap Cut(Bitmap b, int StartX, int StartY, int iWidth, int iHeight)
+        {
+            if (b == null)
+            {
+                return null;
+            }
+            int w = b.Width;
+            int h = b.Height;
+            if (StartX >= w || StartY >= h)
+            {
+                return null;
+            }
+            if (StartX + iWidth > w)
+            {
+                iWidth = w - StartX;
+            }
+            if (StartY + iHeight > h)
+            {
+                iHeight = h - StartY;
+            }
+            try
+            {
+                Bitmap bmpOut = new Bitmap(iWidth, iHeight, PixelFormat.Format24bppRgb);
+                Graphics g = Graphics.FromImage(bmpOut);
+                g.DrawImage(b, new Rectangle(0, 0, iWidth, iHeight), new Rectangle(StartX, StartY, iWidth, iHeight), GraphicsUnit.Pixel);
+                g.Dispose();
+                return bmpOut;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+      
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (fn!=null)
+            {
+                Bitmap bmp = new Bitmap(fn);
+
+                this.pictureBox2.Image = Cut(bmp, 100, 584, 3687, 713);
+                this.pictureBox2.Height = (int) 438 * 713 / 3687;
+            }
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //定义打开的默认文件夹位置
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                files = null;
+                label3.Text = "0";
+                path = openFileDialog1.FileName.Replace(openFileDialog1.SafeFileName, "");
+                files = Directory.GetFiles(path, "*.jpg");
+                int filescount = files.Count();
+                this.label3.Text = filescount.ToString();
+                this.textBox2.Text += path + "  "+"共"+ this.label3.Text+"个jpg文件\r\n";
+                this.label4.Text = "0/" + filescount.ToString();
+                this.progressBar1.Maximum = filescount;//设置最大长度值
+                this.progressBar1.Value = 0;//设置当前值
+                this.progressBar1.Step = 1;//设置没次增长多少
+            }
+        }
+
+
+        private string code;//识别码
+        private string code1;//识别码
+        private int num = 0;
+        public void pic_orc()
+        {
+            int n = files.Count();
+            for (int i = 0; i < n; i++)
+            {
+                string ans = files[i];
+                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+                //识别图片文字
+                var img = new Bitmap(files[i]);
+                var ocr = new TesseractEngine("./tessdata", "chi_sim", EngineMode.Default);
+
+                var page = ocr.Process(Cut(img, 100, 584, 3687, 713));//100, 584, 3687, 713
+                //var page = ocr.Process(Cut(img, 1875, 579, 1917, 693));
+                string ocr_text = page.GetText().Replace(" ", "");
+                page.Dispose();
+                img.Dispose();
+                ocr.Dispose();
+                //查找订单编号
+
+                int start = ocr_text.IndexOf("订单编号");
+                if (start < 0)
+                {
+                    ans += " >>无，订单编号：" + code1 + "_" + num.ToString();
+                    //list.Add(filename + "-" + code);
+                    code = code1 + "_" + num.ToString();
+                    num++;
+                }
+                else
+                {
+                    code = ocr_text.Substring(start + 5, 10);//更新
+                    code1 = code;
+                    ans += " >>订单编号：" + code;
+                    num = 0;
+                    num++;
+                    //list.Add(filename+"-"+code);
+                }
+                sw.Stop();
+                TimeSpan ts2 = sw.Elapsed;
+
+                string dettime = (ts2.TotalMilliseconds / 1000.0).ToString("0.00") + "秒";
+                ans += " 耗时：" + dettime + "\r\n";
+                ocr_text = null;
+               
+             
+                if (this.textBox2.InvokeRequired)//不同线程为true，所以这里是true
+                {
+                    BeginInvoke(new Action(() => { this.textBox2.Text += ans; }));
+                 }
+                if (this.label4.InvokeRequired)//不同线程为true，所以这里是true
+                {
+                    BeginInvoke(new Action(() => { this.label4.Text = (i).ToString()+"/"+n.ToString(); }));
+                }
+                if (this.progressBar1.InvokeRequired)//不同线程为true，所以这里是true
+                {
+                    BeginInvoke(new Action(() => { this.progressBar1.Value += this.progressBar1.Step ; }));
+                }
+                dt.Rows.Add(files[i], code, dettime.ToString());
+            }
+            
+           
+            if (this.button5.InvokeRequired)//不同线程为true，所以这里是true
+            {
+                BeginInvoke(new Action(() => { this.button5.Enabled = true; }));
+            }
+            if (this.button6.InvokeRequired)//不同线程为true，所以这里是true
+            {
+                BeginInvoke(new Action(() => { this.button6.Enabled = true; }));
+            }
+
+            //MessageBox.Show("识别完毕");
+
+            if (!Directory.Exists(path + "/new/"))
+            {
+                Directory.CreateDirectory(path + "/new/");
+
+            }
+            //复制，修改名称
+            for (int i = 0; i < n; i++)
+            {
+                System.IO.File.Copy(dt.Rows[i]["url"].ToString(), path + "/new/" + dt.Rows[i]["code"].ToString() + ".jpg", true);
+                File.AppendAllText(path + "/new/log.txt", "\r\n" + "复制图片" + dt.Rows[i]["url"].ToString().Replace(path,"") + "到/new/" + dt.Rows[i]["code"].ToString() + ".jpg");
+            }
+            MessageBox.Show("复制完毕");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dt.Dispose();
+            dt = new DataTable();
+            dt.Columns.Add("url", Type.GetType("System.String"));
+            dt.Columns.Add("code", Type.GetType("System.String"));
+            dt.Columns.Add("time", Type.GetType("System.String"));
+
+            
+            // var ocr = new TesseractEngine("./tessdata", "chi_sim");
+            //list = new List<string>();
+           // for (int i = 0; i < n; i++)
+           {
+                this.button5.Enabled = false;
+                this.button6.Enabled = false;
+                Task  task = new Task (() => pic_orc());
+                task.Start();
+               // this.textBox2.Text += task.Result;
+               
+            }
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(path + "/new/"))
+            {
+                Directory.CreateDirectory(path + "/new/");
+
+            }
+            int n = files.Count();
+            //复制，修改名称
+            for (int i = 0; i < n; i++)
+            {
+                System.IO.File.Copy(dt.Rows[i]["url"].ToString(), path + "/new/" + dt.Rows[i]["code"].ToString() + ".jpg", true);
+                File.AppendAllText(path + "/new/log.txt", "\r\n" + "复制图片" + dt.Rows[i]["url"].ToString() + "到new/" + dt.Rows[i]["code"].ToString() + ".jpg");
+            }
+            MessageBox.Show("复制完毕");
+        }
+    }
+}
